573baafc8ad229262ad28858c9c3a8960ce1d86a
Suraj Vishwakarma <surajvishwakarma625@gmail.com>
2020-09-22 22:12:17 +0530

    JavaScript Clock

---
+++ Javascript Clock/clock.js
@@ -0,0 +1,25 @@
+function myClock(){
+var today = new Date();
+var hours = today.getHours();
+var minute = today.getMinutes()
+var second = today.getSeconds()
+var amp = "am";
+hours = 7;
+if (hours > 18){
+    document.body.style.backgroundImage = "url('img/night.gif')";
+    document.getElementById("clock").style.color="white"
+}else if (hours <12 && hours>6){
+    document.body.style.backgroundImage = "url('img/morning.gif')";
+    document.getElementById("clock").style.color="orange"
+}
+
+if(hours>12){
+    var hours =hours-12;
+    amp = "pm"
+}
+
+time = hours + ":"+ minute + ":"+second;
+document.getElementById("clock").innerHTML=time+ " "+amp;
+setTimeout(myClock, 1000)
+}
+
