64c66a394b6c2b77cd8abe23980f006fe21f570a
Sahil <sam963dev@gmail.com>
2021-10-15 10:23:08 +0530

    Color change of background and button onclick

---
+++ JavaScript Random Color Generator/Button_Color_Change/random_color_generator.js
@@ -0,0 +1,19 @@
+function generateHex() {
+    return `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")
+    }`;
+}
+
+function changeButtonColor() {
+    const buttonElement = document.querySelector("button");
+    const newColor = generateHex();
+    buttonElement.style.backgroundColor = newColor;
+}
+
+
+// Using ES6 arrow function and String literals.
+// let generateHex = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")`;
+// let changeButtonColor = () => {
+//     const buttonElement = document.querySelector("button");
+//     const newColor = generateHex();
+//     buttonElement.style.backgroundColor = newColor;
+// };
