a6af3145f6585bd9ea0c61200acdebf16cfc4f6b
Harsh0726 <harshanineranjana@gmail.com>
2022-10-25 14:41:24 +0530

    created table for represent of the items

--- JavaScript TODO list generator/script.js
+++ JavaScript TODO list generator/script.js
@@ -1,4 +1,4 @@
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("TD");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
@@ -25,7 +25,7 @@ list.addEventListener('click', function(ev) {
 }, false);
 
 function newElement() {
-    var li = document.createElement("li");
+    var li = document.createElement("tr");
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
@@ -36,6 +36,7 @@ function newElement() {
     }
     document.getElementById("myInput").value = "";
 
+    var td2 = document.createElement('td');
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("Remove");
     span.className = "closeitem btn btn-default pull-right";
