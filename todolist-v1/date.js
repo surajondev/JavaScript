bb4e2d6daab89f993d7e3c9b2620e26060b3abf1
Kodali Bhavana <91609959+kbhavana14@users.noreply.github.com>
2022-10-31 19:09:03 +0530

    Added ToDo List

---
+++ todolist-v1/date.js
@@ -0,0 +1,16 @@
+
+
+exports.getDate  = function () {
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", options);;
+
+};
+
+
